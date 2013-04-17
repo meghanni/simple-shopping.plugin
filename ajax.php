9bf2f7da40037555fcf9cd96082d02c8f3178754
Meghan Nicholas <meghan.nicholas@gmail.com>
2013-04-17 12:57:42 +0100

    Fixed ajax bug when adding to cart

--- ajax.php
+++ ajax.php
@@ -5,7 +5,7 @@ add_action("wp_ajax_nopriv_ssc_cart", "CartMaintenance");
 //http://wp.smashingmagazine.com/2011/10/18/how-to-use-ajax-in-wordpress/
 function CartMaintenance() {
 
-	$id = $_POST['ID'];
+	$id = $_POST['ProductID'];
 
 	//just doing this will perform the add to cart function on the database
 	$cart = new Cart(); 
